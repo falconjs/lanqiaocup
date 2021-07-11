b6f6d00575494214015194c70ae36aaf6cf4b533
James Bai <12628503+falconjs@users.noreply.github.com>
2021-07-11 16:30:30 +0800

    20210530 - 1.4 list 1

---
+++ 1.入门篇/1.0 实验馆/1.3 loop 5.py
@@ -0,0 +1,2 @@
+for looper in range(1,11):
+    print('5 * ' + str(looper) + ' = ' + str(looper * 5))
---
+++ 1.入门篇/1.0 实验馆/1.3 loop 6.py
@@ -0,0 +1,5 @@
+looper = 1
+Num1 = int(input("please enter the first number : "))
+while looper != 11 :
+    print(str(Num1) + ' * ' + str(looper) + ' = ' + str(looper * Num1))
+    looper = looper + 1
