1f0776a1e3c946f6e21b5ee1ef43f12129d82b9c
James Bai <12628503+falconjs@users.noreply.github.com>
2021-11-07 11:17:08 +0800

    update on 20211107

--- 1.入门篇/1.0 实验馆/11.2 nested loop 4.py
+++ 1.入门篇/1.0 实验馆/11.2 nested loop 4.py
@@ -1,7 +1,10 @@
 numBlocks = int(input("How many blocks do you want? A:"))
 for block in range(1, numBlocks + 1):
+    print("block " + str(block))
     for line in range(1, block * 2):
+        print("line " + str(line))
         for star in range(1, (block + line) * 2):
             print("* ", end='')
-        print()
+        print("star " + str(star))
+        # print()
     print()
---
+++ 1.入门篇/1.0 实验馆/12.1 list 1.py
@@ -0,0 +1,20 @@
+student = ['Sam', 'Tom', 'Peter', 'Mini']
+studentnumber = [1, 2, 3 ,4]
+print('students are', student)
+print('students numbers are' , studentnumber)
+
+friend = []
+friend.append('Amy')
+print(friend)
+friend.append('David')
+print(friend)
+
+my_list = [5, 1, 0.4, 'BUG', friend]
+print(my_list)
+
+print(student[2])
+print(my_list[4])
+print(my_list[4][1])
+
+print(studentnumber[1:3])
+print(studentnumber[0:4])
