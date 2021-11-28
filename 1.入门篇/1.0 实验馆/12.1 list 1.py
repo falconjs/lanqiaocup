c4b84743fd326560f5cf9ff78c0ae01e77258a05
James Bai <12628503+falconjs@users.noreply.github.com>
2021-11-28 11:41:08 +0800

    update on 20211128

--- 1.入门篇/1.0 实验馆/12.1 list 1.py
+++ 1.入门篇/1.0 实验馆/12.1 list 1.py
@@ -12,9 +12,36 @@ print(friend)
 my_list = [5, 1, 0.4, 'BUG', friend]
 print(my_list)
 
-print(student[2])
-print(my_list[4])
-print(my_list[4][1])
+# print(student[2])
+# print(my_list[4])
+# print(my_list[4][1])
 
-print(studentnumber[1:3])
-print(studentnumber[0:4])
+# print(studentnumber[1:3])
+# print(studentnumber[0:4])
+
+student[0] = 'Sally'
+print(student)
+student[2] = "Pency"
+print(student)
+
+my_list.extend(['xxx', 3, 'wgr'])
+print(my_list)
+
+friend.insert(1, 'Elex')
+print(friend)
+friend.insert(1, studentnumber)
+print(friend)
+
+student.remove("Tom")
+print(student)
+
+del studentnumber[1]
+print(studentnumber)
+
+lastMy_list = my_list.pop()
+print(my_list)
+print(lastMy_list)
+
+second = my_list.pop(1)
+print(second)
+print(my_list)
